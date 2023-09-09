d46f4f5260916408a27e5f310f4a5e3bcb5b57d6
Chudi <chudiibida3@gmail.com>
2023-09-08 21:23:53 -0400

    added user controller

--- models/goals.js
+++ models/goals.js
@@ -1,18 +1,12 @@
 const mongoose = require('mongoose')
 
 const goalSchema = new mongoose.Schema({
-    goal: { type: String, required: true},
+    goal: { type: String, required: true },
     measurability: { type: String, required: true },
     difficulty: { type: Number, required: true },
     category: { type: String, required: true },
     targetDate: { type: Date, required: true },
-    reason: { type: String, required: false },
-    tasks: [
-        {
-            title: String,
-            minutes: Number
-        }
-    ]
+    reason: { type: String, required: false }
 })
 
 const Goals = mongoose.model('Goal', goalSchema)
